552d94536a26f8a46c1fb27bc464f8ff095003f7
Abhiek187 <achaudhuri2011@yahoo.com>
2021-04-28 19:58:11 -0400

    Improve the formatting

--- ref-conv.py
+++ ref-conv.py
@@ -5,20 +5,21 @@
 import argparse
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import numpy as np
+from time import time
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-from torchvision import datasets, transforms
-import time
-import numpy as np
 from torch.optim.lr_scheduler import StepLR
+from torchvision import datasets, transforms
 
 mpl.use('Agg')
 
 
 class Flatten(nn.Module):
     def forward(self, input):
+        # Flatten the 4-dimensional array into 2 dimensions
         return input.view(input.size(0), -1)
 
 
@@ -27,81 +28,95 @@ CNN model:
 Input: 28x28, 1 channel
 Conv1: 32 3x3 filters, stride 1 -> 32x26x26 output, ReLU activation
 Conv2: 64 3x3 filters, stride 1 -> 64x24x24 output, ReLU activation
-MaxPool: downscaling by 2 -> 64x12x12 output
-Flatten: 9216 layers
+MaxPool: downscale by 2 -> 64x12x12 output
+Flatten: 64*12*12 = 9216 layers
 FC1: 9216 -> 128 layers, ReLU activation
 FC2: 128 -> 10 layers
 """
 Model = torch.nn.Sequential(
-    nn.Conv2d(1, 32, 3, 1),
+    nn.Conv2d(1, 32, kernel_size=3),
     nn.ReLU(),
-    nn.Conv2d(32, 64, 3, 1),
+    nn.Conv2d(32, 64, kernel_size=3),
     nn.ReLU(),
-    nn.MaxPool2d(kernel_size=2),
+    nn.MaxPool2d(kernel_size=2),  # stride = kernel_size
     Flatten(),
     nn.Linear(9216, 128),
     nn.ReLU(),
-    nn.Linear(128, 10))
+    nn.Linear(128, 10)
+)
 
 
 def train(model, device, train_loader, optimizer, loss, epoch):
+    # Set the model to training mode
     model.train()
-    count = 0
+    # Store all the loss values for the graph
     tmp_loss = []
+
     for batch_idx, (data, target) in enumerate(train_loader):
+        # Forward pass
         data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
         output = model(data)
+
         if loss == 'CE':
             # CrossEntropy Loss
             loss_fn = nn.CrossEntropyLoss()
-        if loss == 'MSE':
+        else:
             # MSE Loss
             # Prepare for one-hot labels
-            y_onehot = target.numpy()
-            y_onehot = (np.arange(10) == y_onehot[:, None]).astype(np.float32)
-            target = torch.from_numpy(y_onehot)
+            y_one_hot = target.numpy()
+            y_one_hot = (np.arange(10) == y_one_hot[:, None]).astype(np.float32)
+            target = torch.from_numpy(y_one_hot)
             loss_fn = nn.MSELoss()
 
+        # Compute the loss and perform backward propagation
         loss_ = loss_fn(output, target)
         loss_.backward()
         optimizer.step()
+
+        # Print the progress every log interval
         if batch_idx % FLAGS.log_interval == 0:
             print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(train_loader.dataset),
-                       100. * batch_idx / len(train_loader), loss_.item()))
+                100. * batch_idx / len(train_loader), loss_.item()))
             tmp_loss.append(loss_.item())
+
     return tmp_loss
 
 
 def test(model, device, test_loader):
+    # Set the model to testing mode
     model.eval()
     test_loss = 0
     correct = 0
+
     with torch.no_grad():
         for data, target in test_loader:
+            # Compute the loss after a forward pass
             data, target = data.to(device), target.to(device)
             output = model(data)
             test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
 
             pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
             correct += pred.eq(target.view_as(pred)).sum().item()
-    test_loss /= len(test_loader.dataset)
 
+    test_loss /= len(test_loader.dataset)
     print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
         test_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
 
 
 def main():
-    # set device
+    # Use either the CPU or GPU
     no_cuda = False
     use_cuda = not no_cuda and torch.cuda.is_available()
     device = torch.device("cuda" if use_cuda else "cpu")
 
-    # load MNIST dataset
+    # Load the MNIST dataset
+    # x_train: 60Kx28x28, y_train: 60K, x_test: 10Kx28x28, y_test: 10K
     batch_size = FLAGS.train_batch_size
     test_batch_size = FLAGS.test_batch_size
+
     train_loader = torch.utils.data.DataLoader(
         datasets.MNIST(FLAGS.data_dir, train=True, download=True,
                        transform=transforms.Compose([
@@ -109,6 +124,7 @@ def main():
                            transforms.Normalize((0.1307,), (0.3081,))
                        ])),
         batch_size=batch_size, shuffle=True)
+
     test_loader = torch.utils.data.DataLoader(
         datasets.MNIST(FLAGS.data_dir, train=False, transform=transforms.Compose([
             transforms.ToTensor(),
@@ -116,38 +132,51 @@ def main():
         ])),
         batch_size=test_batch_size, shuffle=True)
 
-    # set optimizer
+    # Set the optimizer to update the weights and bias
     lr = FLAGS.lr
     model = Model.to(device)
     optimizer = optim.SGD(model.parameters(), lr=lr)
-    time0 = time.time()
     # Training settings
     epochs = FLAGS.epochs
     loss = FLAGS.loss_fn
+
     if FLAGS.start_checkpoint:
         ckpt = torch.load(FLAGS.start_checkpoint)
         model.load_state_dict(ckpt)
-    # start training
-    time0 = time.time()
+
+    # Start training
+    time0 = time()
     loss_values = []
+    # Decay the learning rate 70% every epoch
     scheduler = StepLR(optimizer, step_size=1, gamma=0.7)
-    # x_train: 60Kx28x28, y_train: 60K, x_test: 10Kx28x28, y_test: 10K
+
     for epoch in range(1, epochs + 1):
         _ = train(model, device, train_loader, optimizer, loss, epoch)
         loss_values.extend(_)
         test(model, device, test_loader)
         scheduler.step()
+
     if FLAGS.save_model:
         torch.save(model.state_dict(), FLAGS.save_checkpoint)
 
-    time1 = time.time()
-    fig = plt.figure()
-    plt.plot(loss_values)
+    time1 = time()
+    # Plot the training loss
+    plt.figure()
+    plt.title("Training Loss Over Epochs")
+    plt.xlabel("Epoch")
+    plt.ylabel("Training Loss")
+    # Align the x values to the correct epoch number
+    epoch_values = np.arange(1, len(loss_values) + 1, dtype=np.float)
+    epoch_values *= FLAGS.train_batch_size * FLAGS.log_interval
+    epoch_values /= len(train_loader.dataset)
+    plt.xticks(np.arange(1, 11))
+    plt.plot(epoch_values, loss_values)
     plt.savefig(FLAGS.loss_fig)
     print('Training and Testing total execution time is: %s seconds ' % (time1 - time0))
 
 
 if __name__ == '__main__':
+    # Read all the command line arguments, or supply default values
     parser = argparse.ArgumentParser()
     parser.add_argument('--data_dir', type=str, default='downloads',
                         help='Where to download the MNIST data to')
@@ -171,5 +200,5 @@ if __name__ == '__main__':
                         help='Save the trained model.')
     parser.add_argument('--loss_fig', type=str, default='downloads/loss_curve.png',
                         help='Where to save the plotted training loss curve.')
-    FLAGS, unparsed = parser.parse_known_args()
+    FLAGS, _ = parser.parse_known_args()
     main()
