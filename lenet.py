2d6af191a3951e316229d5692837e5fa9ef37c07
Abhiek187 <achaudhuri2011@yahoo.com>
2021-04-30 20:34:45 -0400

    Convert to a LeNet model

--- lenet.py
+++ lenet.py
@@ -14,36 +14,45 @@ import torch.optim as optim
 from torch.optim.lr_scheduler import StepLR
 from torchvision import datasets, transforms
 
-mpl.use('Agg')
+mpl.use("Agg")
 
 
-class Flatten(nn.Module):
-    def forward(self, input):
-        # Flatten the 4-dimensional array into 2 dimensions
-        return input.view(input.size(0), -1)
+class LeNet(nn.Module):
+    """
+    LeNet model:
+    Input: 28x28, 1 channel
+    Conv1: 6 5x5 filters, padding 2 -> 6x28x28 output, Tanh activation
+    MaxPool1: downscale by 2 -> 6x14x14 output
+    Conv2: 16 5x5 filters -> 16x10x10 output, Tanh activation
+    MaxPool2: downscale by 2 -> 16x5x5 output
+    Conv3: 120 5x5 filters -> 120x1x1 output, Tanh activation
+    Flatten: 120 layers
+    FC1: 120 -> 84 layers, Tanh activation
+    FC2: 84 -> 10 layers
+    """
+    def __init__(self):
+        super(LeNet, self).__init__()
 
+        self.conv = nn.Sequential(
+            nn.Conv2d(1, 6, kernel_size=5, padding=2),
+            nn.Tanh(),
+            nn.MaxPool2d(kernel_size=2),
+            nn.Conv2d(6, 16, kernel_size=5),
+            nn.Tanh(),
+            nn.MaxPool2d(kernel_size=2),
+            nn.Conv2d(16, 120, kernel_size=5),
+            nn.Tanh()
+        )
 
-"""
-CNN model:
-Input: 28x28, 1 channel
-Conv1: 32 3x3 filters, stride 1 -> 32x26x26 output, ReLU activation
-Conv2: 64 3x3 filters, stride 1 -> 64x24x24 output, ReLU activation
-MaxPool: downscale by 2 -> 64x12x12 output
-Flatten: 64*12*12 = 9216 layers
-FC1: 9216 -> 128 layers, ReLU activation
-FC2: 128 -> 10 layers
-"""
-Model = torch.nn.Sequential(
-    nn.Conv2d(1, 32, kernel_size=3),
-    nn.ReLU(),
-    nn.Conv2d(32, 64, kernel_size=3),
-    nn.ReLU(),
-    nn.MaxPool2d(kernel_size=2),  # stride = kernel_size
-    Flatten(),
-    nn.Linear(9216, 128),
-    nn.ReLU(),
-    nn.Linear(128, 10)
-)
+        self.fc = nn.Sequential(
+            nn.Linear(120, 84),
+            nn.Tanh(),
+            nn.Linear(84, 10)
+        )
+
+    def forward(self, x):
+        out = self.conv(x).view(-1, 120)
+        return self.fc(out)
 
 
 def train(model, device, train_loader, optimizer, loss, epoch):
@@ -58,7 +67,7 @@ def train(model, device, train_loader, optimizer, loss, epoch):
         optimizer.zero_grad()
         output = model(data)
 
-        if loss == 'CE':
+        if loss == "CE":
             # CrossEntropy Loss
             loss_fn = nn.CrossEntropyLoss()
         else:
@@ -76,15 +85,14 @@ def train(model, device, train_loader, optimizer, loss, epoch):
 
         # Print the progress every log interval
         if batch_idx % FLAGS.log_interval == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader), loss_.item()))
+            print(f"Train Epoch: {epoch} [{batch_idx * len(data)}/{len(train_loader.dataset)} "
+                  f"({100. * batch_idx / len(train_loader):.0f}%)]\tLoss: {loss_.item():.6f}")
             tmp_loss.append(loss_.item())
 
     return tmp_loss
 
 
-def test(model, device, test_loader):
+def test(model, device, test_loader, is_training=False):
     # Set the model to testing mode
     model.eval()
     test_loss = 0
@@ -95,15 +103,21 @@ def test(model, device, test_loader):
             # Compute the loss after a forward pass
             data, target = data.to(device), target.to(device)
             output = model(data)
-            test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
+            test_loss += F.nll_loss(output, target, reduction="sum").item()  # sum up batch loss
 
             pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
             correct += pred.eq(target.view_as(pred)).sum().item()
 
     test_loss /= len(test_loader.dataset)
-    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-        test_loss, correct, len(test_loader.dataset),
-        100. * correct / len(test_loader.dataset)))
+
+    if is_training:
+        print(f"\nTraining set: Average loss: {test_loss:.4f}, Accuracy: "
+              f"{correct}/{len(test_loader.dataset)} "
+              f"({100. * correct / len(test_loader.dataset):.0f}%)\n")
+    else:
+        print(f"\nTest set: Average loss: {test_loss:.4f}, Accuracy: "
+              f"{correct}/{len(test_loader.dataset)} "
+              f"({100. * correct / len(test_loader.dataset):.0f}%)\n")
 
 
 def main():
@@ -134,7 +148,7 @@ def main():
 
     # Set the optimizer to update the weights and bias
     lr = FLAGS.lr
-    model = Model.to(device)
+    model = LeNet().to(device)
     optimizer = optim.SGD(model.parameters(), lr=lr)
     # Training settings
     epochs = FLAGS.epochs
@@ -153,6 +167,7 @@ def main():
     for epoch in range(1, epochs + 1):
         _ = train(model, device, train_loader, optimizer, loss, epoch)
         loss_values.extend(_)
+        test(model, device, train_loader, is_training=True)
         test(model, device, test_loader)
         scheduler.step()
 
@@ -172,33 +187,33 @@ def main():
     plt.xticks(np.arange(1, 11))
     plt.plot(epoch_values, loss_values)
     plt.savefig(FLAGS.loss_fig)
-    print('Training and Testing total execution time is: %s seconds ' % (time1 - time0))
+    print(f"Training and Testing total execution time is: {time1 - time0} seconds ")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Read all the command line arguments, or supply default values
     parser = argparse.ArgumentParser()
-    parser.add_argument('--data_dir', type=str, default='downloads',
-                        help='Where to download the MNIST data to')
-    parser.add_argument('--train_batch_size', type=int, default=128, metavar='N',
-                        help='input batch size for training')
-    parser.add_argument('--test_batch_size', type=int, default=10000, metavar='N',
-                        help='input batch size for testing')
-    parser.add_argument('--loss_fn', type=str, default='CE',
-                        help='select one loss function from CE and MSE')
-    parser.add_argument('--epochs', type=int, default=10, metavar='N',
-                        help='number of epochs to train')
-    parser.add_argument('--lr', type=float, default=0.10, metavar='LR',
-                        help='learning rate ')
-    parser.add_argument('--log_interval', type=int, default=100, metavar='N',
-                        help='how many batches to wait before logging training status')
-    parser.add_argument('--start_checkpoint', type=str, default=None,  # or "mnist_cnn.pth",
-                        help='If specified, restore this pretrained model before any training.')
-    parser.add_argument('--save_model', action='store_true', default=False,
-                        help='For Saving the current Model')
-    parser.add_argument('--save_checkpoint', type=str, default="downloads/mnist_cnn.pth",
-                        help='Save the trained model.')
-    parser.add_argument('--loss_fig', type=str, default='downloads/loss_curve.png',
-                        help='Where to save the plotted training loss curve.')
+    parser.add_argument("--data_dir", type=str, default="downloads",
+                        help="Where to download the MNIST data to")
+    parser.add_argument("--train_batch_size", type=int, default=128, metavar="N",
+                        help="input batch size for training")
+    parser.add_argument("--test_batch_size", type=int, default=10000, metavar="N",
+                        help="input batch size for testing")
+    parser.add_argument("--loss_fn", type=str, default="CE",
+                        help="select one loss function from CE and MSE")
+    parser.add_argument("--epochs", type=int, default=10, metavar="N",
+                        help="number of epochs to train")
+    parser.add_argument("--lr", type=float, default=0.10, metavar="LR",
+                        help="learning rate ")
+    parser.add_argument("--log_interval", type=int, default=100, metavar="N",
+                        help="how many batches to wait before logging training status")
+    parser.add_argument("--start_checkpoint", type=str, default=None,  # or "mnist_cnn.pth",
+                        help="If specified, restore this pretrained model before any training.")
+    parser.add_argument("--save_model", action="store_true", default=True,
+                        help="For Saving the current Model")
+    parser.add_argument("--save_checkpoint", type=str, default="downloads/mnist_cnn.pth",
+                        help="Save the trained model.")
+    parser.add_argument("--loss_fig", type=str, default="downloads/loss_curve.png",
+                        help="Where to save the plotted training loss curve.")
     FLAGS, _ = parser.parse_known_args()
     main()
